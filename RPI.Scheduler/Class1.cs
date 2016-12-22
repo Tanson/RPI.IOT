11993b122f498da34d061bde6a38a038a19e3799
Tanson <tansenen@163.com>
2016-12-23 01:11:14 +0800

    update

---
+++ RPI.Scheduler/Class1.cs
@@ -0,0 +1,48 @@
+﻿using Quartz;
+using Quartz.Impl;
+using Quartz.Impl.Triggers;
+using RPI.Scheduler.Job;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPI.Scheduler
+{
+    public class JobService
+    {
+        
+        public static void Initially()
+        {
+            ////工厂
+            //ISchedulerFactory factory = new StdSchedulerFactory();
+            ////启动
+            //IScheduler scheduler = factory.GetScheduler();
+            //scheduler.Start();
+            ////描述工作
+            //IJobDetail jobDetail = new JobDetailImpl("mylittlejob", null, typeof(MyJob));
+            ////触发器
+            //ISimpleTrigger trigger = new SimpleTriggerImpl("mytrigger",
+            //    null,
+            //    DateTime.Now,
+            //    null,
+            //    SimpleTriggerImpl.RepeatIndefinitely,
+            //    TimeSpan.FromSeconds(10));
+            ////执行
+            //scheduler.ScheduleJob(jobDetail, trigger);
+
+
+            IScheduler scheduler = StdSchedulerFactory.GetDefaultScheduler();
+            scheduler.Start();
+            IJobDetail job = JobBuilder.Create<DHT11Job>().Build();
+            ITrigger trigger = TriggerBuilder.Create()
+              .WithIdentity("triggerName", "groupName")
+              .WithSimpleSchedule(t =>
+                t.WithIntervalInSeconds(5)
+                 .RepeatForever())
+                 .Build();
+            scheduler.ScheduleJob(job, trigger);
+        }
+    }
+}
